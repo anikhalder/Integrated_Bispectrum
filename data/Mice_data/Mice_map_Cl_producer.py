70a6c8ecd1f144ca7e45f6774a202a5a3363d1c0
Anik Halder <halder.anik04@gmail.com>
2019-05-02 20:48:09 +0200

    Add Mice map data and power spectrum analysis scripts and plots

---
+++ data/Mice_data/Mice_map_Cl_producer.py
@@ -0,0 +1,14 @@
+import numpy as np
+import healpy as hp
+
+
+number_field_mice = hp.fitsfunc.read_map('./mice_number_field.fits')
+mean_number = np.mean(number_field_mice)
+density_field_mice = (number_field_mice - mean_number) / mean_number
+
+cl = hp.sphtfunc.anafast(density_field_mice)
+l = np.arange(0,cl.size,1)
+
+dat = np.array([l[2:], cl[2:]])
+dat = dat.T
+np.savetxt('Cell_mice_map.dat', dat, delimiter = ' ')
